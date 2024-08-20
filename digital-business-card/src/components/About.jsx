262954a40cde864b7f9348492b22e9b0d6c8b747
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-20 00:48:10 -0400

    Add styles for `About` component.

--- digital-business-card/src/components/About.jsx
+++ digital-business-card/src/components/About.jsx
@@ -3,10 +3,7 @@ export default function About() {
         <div className="about--container">
             <h2>About</h2>
             <p>
-                I am a frontend developer with a particular interest in making
-                things simple and automating daily tasks. I try to keep up with
-                security and best practices, and am always looking for new
-                things to learn.
+                I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
             </p>
         </div>
     )
