fbbd89a1fe91f8bb4bacb4d01e34eec554212e51
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-21 18:56:08 -0400

    Update Joke.

--- render-jokes/src/components/Joke.jsx
+++ render-jokes/src/components/Joke.jsx
@@ -2,8 +2,10 @@
 export default function Joke({setup, punchline}) {
     return (
         <div>
-            {setup && <h1>{setup}</h1>}
-            <h2>{punchline}</h2>
+            {/* conditional rendering */}
+            {setup && <h2>{setup}</h2>}
+            <p>{punchline}</p>
+            <hr/>
         </div>
     )
 }
