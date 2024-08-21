6bfb8c367d808473d6168749bb5c020bd53657b4
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-21 13:45:23 -0400

    Add Card component and styling.

--- airbnb-experiences-clone/src/App.jsx
+++ airbnb-experiences-clone/src/App.jsx
@@ -1,11 +1,13 @@
-import Hero from '../src/components/Hero'
+import Card from '../src/components/Card'
+// import Hero from '../src/components/Hero'
 import Navbar from '../src/components/Navbar'
 
 function App() {
   return (
     <div className='app-container'>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
+      <Card />
     </div>
   )
 }
---
+++ airbnb-experiences-clone/src/components/Card.jsx
@@ -0,0 +1,17 @@
+export default function Card() {
+    return (
+        <div className="card--container">
+            <img src="../assets/katie-zaferes.png" className="card--img" />
+            <div className="card--details">
+                <div className="card--stats">
+                    <img src="../assets/star.png" className="card--img-star" />
+                    <span>5.0</span>
+                    <span className="grey">(6) &bull;</span>
+                    <span className="grey">USA</span>
+                </div>
+                <p>Life lessons with Katie Zaferes</p>
+                <p><span className="bold">From $136</span> / person</p>
+            </div>
+        </div>
+    )
+}
