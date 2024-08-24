9381f459b6f4f156e309d07bbc65016e4021d6b8
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-24 12:43:58 -0400

    Change `navbar` term to `header`.

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from './components/Navbar.jsx'
+import Header from './components/Header.jsx'
 
 function App() {
   return (
     <div className='app-container'>
-      <Navbar />
+      <Header />
     </div>
   )
 }
---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header () {
+    return (
+        <nav className="header--container">
+            <img
+                src="../assets/troll-face.png"
+                width="40"
+                height="40"
+                alt="Troll face image"
+                className="header--img"
+            />
+            <h1 className="header--title">Meme Generator</h1>
+            <h2 className="header--project">React Course - Project 3</h2>
+        </nav>
+    )
+}
