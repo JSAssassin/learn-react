fa1a1dc8277c9e79649bcf5906da80476137f3f3
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-26 11:47:30 -0400

    Add labels and adjust styles.

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,16 +2,34 @@ export default function Meme () {
     return (
         <main className="meme--container">
             <form className="meme--form">
-                <input
-                    type="text"
-                    className="meme--form-input"
-                    placeholder="Top text"
-                />
-                <input
-                    type="text"
-                    className="meme--form-input"
-                    placeholder="Bottom text"
-                />
+                <div className="meme--form-input-group">
+                    <label
+                        htmlFor="meme--form-top-text"
+                        className="meme--form-label"
+                    >
+                        Top text
+                    </label>
+                    <input
+                        id="meme--form-top-text"
+                        type="text"
+                        className="meme--form-input"
+                        placeholder="Shut up"
+                    />
+                </div>
+                <div className="meme--form-input-group">
+                    <label
+                        htmlFor="meme--form-bottom-text"
+                        className="meme--form-label"
+                    >
+                        Bottom text
+                    </label>
+                    <input
+                        id="meme--form-bottom-text"
+                        type="text"
+                        className="meme--form-input"
+                        placeholder="And take my money"
+                    />
+                </div>
                 <button className="meme--form-btn">
                     Get a new meme image 🖼
                 </button>
