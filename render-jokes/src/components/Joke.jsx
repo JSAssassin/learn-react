b8c48ba63b678d402a40bdc62b27200f6ed108ba
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-27 18:46:01 -0400

    Add button to show and hide punchlines.

--- render-jokes/src/components/Joke.jsx
+++ render-jokes/src/components/Joke.jsx
@@ -1,10 +1,19 @@
+import {useState} from 'react';
 
 export default function Joke({setup, punchline}) {
+    const [isShown, setIsShown] = useState(false)
+    function toggle() {
+        setIsShown(prevVal => !prevVal)
+    }
+    const styles = {
+        display: isShown ? 'block' : 'none'
+    }
     return (
         <div>
             {/* conditional rendering */}
             {setup && <h2>{setup}</h2>}
-            <p>{punchline}</p>
+            <p style={styles}>{punchline}</p>
+            <button onClick={toggle}>{isShown ? 'Hide' : 'Show'} Punchline</button>
             <hr/>
         </div>
     )
