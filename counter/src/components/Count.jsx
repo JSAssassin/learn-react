423538aa2ac759e4bc37c4e9770e8bdfd7f793d5
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-27 14:18:00 -0400

    Move Count display as its own component.

---
+++ counter/src/components/Count.jsx
@@ -0,0 +1,8 @@
+
+export default function Count(props) {
+    return (
+        <div className='counter--display'>
+            <h1>{props.count}</h1>
+        </div>
+    )
+}
--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import Count from './Count'
 
 export default function Counter () {
     const [count, setCount] = useState(0);
@@ -14,9 +15,7 @@ export default function Counter () {
             <button className='counter--minus' onClick={subtract}>
                 -
             </button>
-            <div className='counter--display'>
-                <h1>{count}</h1>
-            </div>
+            <Count count={count} />
             <button className='counter--plus' onClick={add}>
                 +
             </button>
