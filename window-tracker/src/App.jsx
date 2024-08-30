9fb64d545a244a94f6df1ca99022daad5c7fc1f9
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-30 14:14:00 -0400

    Add initial files for window tracker.

---
+++ window-tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import WindowTracker from './components/WindowTracker.jsx'
+
+function App() {
+  return (
+    <div className='app-container'>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+export default App
