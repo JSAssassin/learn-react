dc630c10c0c4f2484fab19484c3f5fa5982b554b
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-08-30 14:22:12 -0400

    Render `WindowTracker`.

--- window-tracker/src/App.jsx
+++ window-tracker/src/App.jsx
@@ -3,8 +3,7 @@ import WindowTracker from './components/WindowTracker.jsx'
 function App() {
   return (
     <div className='app-container'>
-      <Header />
-      <Meme />
+      <WindowTracker />
     </div>
   )
 }
