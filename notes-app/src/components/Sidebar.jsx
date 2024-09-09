2ee8f51f96595bb0fb469be7cb0926e60952f96f
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-09-09 12:33:53 -0400

    Fix note summary title.

--- notes-app/src/components/Sidebar.jsx
+++ notes-app/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 
 export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => {
+    const noteElements = props.notes.map((note) => {
         return (
             <div key={note.id}>
                 <div
@@ -8,7 +8,7 @@ export default function Sidebar(props) {
                       'selected' : ''}`}
                     onClick={() => props.setCurrentNoteId(note.id)}
                 >
-                    <h4 className="text-snippet">Note {index + 1}</h4>
+                    <h4 className="text-snippet">{note.body.split('\n')[0]}</h4>
                 </div>
             </div>
         )
