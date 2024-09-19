ba2b461ae6a1db82a7e4e687bab80b219ed098a7
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-09-19 15:31:18 -0400

    Add Die component.

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,7 +1,21 @@
+import Dice from './components/Die'
 
 function App() {
   return (
-    <main></main>
+    <main>
+      <div className="dice-container">
+        <Dice value="1"/>
+        <Dice value="1"/>
+        <Dice value="1"/>
+        <Dice value="1"/>
+        <Dice value="1"/>
+        <Dice value="1"/>
+        <Dice value="1"/>
+        <Dice value="1"/>
+        <Dice value="1"/>
+        <Dice value="1"/>
+      </div>
+    </main>
   )
 }
 
