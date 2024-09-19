ad590e1b2a62cadfc262ba1bc6fbb041eb7b3901
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-09-19 16:22:44 -0400

    Map dice array to die elements.

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,19 +1,23 @@
-import Dice from './components/Die'
+import Die from './components/Die'
+import React from 'react'
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+  function allNewDice() {
+    let result = [];
+    for(let i = 1; i <=10; i++) {
+        const value = Math.floor(Math.random() * 6 + 1);
+        result.push(value)
+    }
+    return result;
+  }
+  const diceElements = dice.map(
+    (die, index) => <Die key={index + 1} value={die} />);
+
   return (
     <main>
       <div className="dice-container">
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
+        {diceElements}
       </div>
     </main>
   )
