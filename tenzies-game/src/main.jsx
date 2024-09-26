7109726bb0afc6f8fd9dc9966337d2900002f496
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-09-25 23:59:33 -0400

    Update `react` and `react-dom` to latest versions.

--- tenzies-game/src/main.jsx
+++ tenzies-game/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 import './index.css'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
-)
+  </StrictMode>
+);
