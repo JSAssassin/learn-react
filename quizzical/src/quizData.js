633b0307153146b2192d85f69bcc8929373912d5
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-09-28 14:40:36 -0400

    Add quiz data.

---
+++ quizzical/src/quizData.js
@@ -0,0 +1,32 @@
+export default {
+    "success": true,
+    "data": {
+        "quiz": [
+            {
+                question: 'How would one say goodbye in Spanish?',
+                answerOptions: ['Adiós', 'Hola', 'Au Revoir', 'Salir'],
+                correctAnswer: 'Adiós'
+            },
+            {
+                question: 'Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?',
+                answerOptions: ['Cabbage Patch Kids', 'Transformers', 'Care Bears', 'Rubik’s Cube'],
+                correctAnswer: 'Care Bears'
+            },
+            {
+                question: 'What is the hottest planet in our Solar System?',
+                answerOptions: ['Mercury', 'Venus', 'Mars', 'Saturn'],
+                correctAnswer: 'Venus'
+            },
+            {
+                question: 'In which country was the caesar salad invented?',
+                answerOptions: ['Italy', 'Portugal', 'Mexico', 'France'],
+                correctAnswer: 'Mexico'
+            },
+            {
+                question: 'How Many Hearts Does An Octopus Have?',
+                answerOptions: ['One', 'Two', 'Three', 'Four'],
+                correctAnswer: 'Three'
+            }
+        ]
+    }
+}
