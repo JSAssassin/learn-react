254423de7cfa90b9ca7799dcdeb04833132f1613
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-10-05 14:26:25 -0400

    Handle answer selection.

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,21 +1,33 @@
 import topRightBlob from './assets/top-right-blob.png';
 import bottomLeftBlob from './assets/bottom-left-blob.png';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Intro from './components/Intro';
 import Question from './components/Question';
 import questionsData from './questionsData';
 
 function App() {
   const [started, setStarted] = useState(false);
+  const [answers, setAnswers] = useState({});
+
   function startQuiz() {
     setStarted(true);
   }
+
+  function handleOptionSelect(questionId, answerIndex) {
+    setAnswers(prevValues => {
+      return {...prevValues, [questionId]: answerIndex};
+    });
+  }
+
+  useEffect(() => {
+    console.log('answers: ', answers);
+  })
   const {data: {questions: quizQuestions}} = questionsData;
   const quiz = quizQuestions.map(quizQuestion =>
     <Question
       key={quizQuestion.id}
-      question={quizQuestion.question}
-      answerOptions={quizQuestion.answerOptions}
+      quizQuestion={quizQuestion}
+      handleOptionSelect={handleOptionSelect}
     />
   )
   return (
--- quizzical/src/components/Question.jsx
+++ quizzical/src/components/Question.jsx
@@ -1,12 +1,26 @@
 export default function Question(props) {
-    const {answerOptions, question} = props;
-    const options = answerOptions.map((option, index) =>
-        <button  key={index} type="button" >{option}</button>
-    )
+    const {quizQuestion, handleOptionSelect} = props;
+    const options = quizQuestion.answerOptions.map((option, index) => {
+        const id = `${quizQuestion.id}-${index}`;
+        return (
+            <div  key={index}>
+                <input
+                    id={id}
+                    type="radio"
+                    name={`quiz-${quizQuestion.id}`}
+                    value={index}
+                    onClick={() => handleOptionSelect(quizQuestion.id, index)}
+                />
+                <label htmlFor={id}>
+                    {option}
+                </label>
+            </div>
+        )
+    })
     return (
         <div className="question-container">
-            <h1 className="question">{question}</h1>
-            <div  className="answerOptions">
+            <h1 className="question">{quizQuestion.question}</h1>
+            <div className="answerOptions">
                 {options}
             </div>
         </div>
