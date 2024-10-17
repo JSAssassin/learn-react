b245c0ec5cec6f8e165f66320e75b208e7f262c6
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-10-17 11:44:36 -0400

    Change name `quizzical` to `mindflex`.

--- mindflex/src/components/Intro.jsx
+++ mindflex/src/components/Intro.jsx
@@ -2,9 +2,9 @@ export default function Intro(props) {
     const {startQuiz} = props;
     return (
         <main className='intro-container'>
-            <h1 className='intro-title'>Quizzical</h1>
+            <h1 className='intro-title'>MindFlex</h1>
             <p className='intro-description'>
-                Welcome to Quizzical! Put your knowledge to the test with a fun
+                Welcome to MindFlex! Put your mind to the test with a fun
                 and engaging quiz game where you’ll tackle a variety of
                 questions. Each question comes with a couple of answer options
                 — choose wisely! If you guess correctly, you earn a point; if
@@ -15,7 +15,7 @@ export default function Intro(props) {
                 className='intro-start-btn'
                 onClick={startQuiz}
             >
-                Start quiz
+                Start
             </button>
         </main>
     )
