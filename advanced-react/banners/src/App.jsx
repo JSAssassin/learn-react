d2386b35f73ffe15746c1f0066772de55e324fc6
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-10-28 14:29:37 -0400

    Initial files for banners.

---
+++ advanced-react/banners/src/App.jsx
@@ -0,0 +1,12 @@
+import Banner from './components/Banner'
+
+function App() {
+  return (
+    <>
+      <h1 className='title'>Banners</h1>
+      <Banner>Banner</Banner>
+    </>
+  )
+}
+
+export default App
