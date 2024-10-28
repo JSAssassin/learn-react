d98cea30d3972b6564a7999088b10ba1d50af25d
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-10-28 14:07:54 -0400

    Update Badge to add size and additional styles support.

--- advanced-react/badges/src/App.jsx
+++ advanced-react/badges/src/App.jsx
@@ -3,10 +3,10 @@ import Badge from './components/Badge'
 function App() {
   const colors = ['gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
   const squareBadges = colors.map(
-    color => <Badge key={color} type='square' color={color}>Badge</Badge>
+    color => <Badge key={`${color}-square`} type='square' color={color} size='medium'>Badge</Badge>
   )
   const pillBadges = colors.map(
-    color => <Badge key={color} type='pill' color={color}>Badge</Badge>
+    color => <Badge key={`${color}-pill`} type='pill' color={color} size='small'>Badge</Badge>
   )
   return (
     <>
@@ -14,10 +14,16 @@ function App() {
       <div className='square-badges-container'>
         <p className='badges-title'>Square</p>
         {squareBadges}
+        <Badge type='square' color='red' size='small'>Small</Badge>
+        <Badge type='square' color='blue' size='medium'>Medium</Badge>
+        <Badge type='square' color='green' size='large'>Large</Badge>
       </div>
       <div className='pill-badges-container'>
         <p className='badges-title'>Pill</p>
         {pillBadges}
+        <Badge type='pill' color='red' size='small'>Small</Badge>
+        <Badge type='pill' color='blue' size='medium'>Medium</Badge>
+        <Badge type='pill' color='green' size='large'>Large</Badge>
       </div>
     </>
   )
--- advanced-react/badges/src/components/Badge.jsx
+++ advanced-react/badges/src/components/Badge.jsx
@@ -1,9 +1,18 @@
 import classnames from 'classnames';
 
-export default function Badge({ children, type = 'square', color = 'gray' }) {
-    const typeClass = type;
-    const colorClass = color;
-    const allClasses = classnames(typeClass, colorClass);
+export default function Badge({
+    children,
+    type = 'square',
+    color = 'gray',
+    size = 'medium',
+    className = ''
+}) {
+    const allClasses = classnames(
+        `badge-${type}`,
+        `badge-${color}`,
+        `badge-${size}`,
+        className
+    );
     return (
         <div className={allClasses}>{children}</div>
     )
