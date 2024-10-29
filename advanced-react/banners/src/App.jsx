f3d655a9f56ea4920f3a8c6e40edf6fba7d75fe8
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-10-29 12:50:01 -0400

    Update banner to take `title`, `text`, and `status` props.

--- advanced-react/banners/src/App.jsx
+++ advanced-react/banners/src/App.jsx
@@ -4,7 +4,56 @@ function App() {
   return (
     <>
       <h1 className='title'>Banners</h1>
-      <Banner>Banner</Banner>
+      <div className="banners-container">
+        <div className="banner-titles">
+          <h1>Success</h1>
+          <h1>Warning</h1>
+          <h1>Error</h1>
+          <h1>Neutral</h1>
+        </div>
+        <div className="banner-section">
+          <h1 className="section-header multiline">Multi Line</h1>
+          <Banner
+            title='Congratulations!'
+            text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.'
+            status='success'
+          />
+          <Banner
+            title='Attention'
+            text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.'
+            status='warning'
+          />
+          <Banner
+            title='There is a problem with your application'
+            text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.'
+            status='error'
+          />
+          <Banner
+            title='Update available'
+            text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.'
+            status='neutral'
+          />
+        </div>
+        <div className="banner-section">
+          <h1 className="section-header singleline">Single Line</h1>
+          <Banner
+            title='Congratulations!'
+            status='success'
+          />
+          <Banner
+            title='Attention'
+            status='warning'
+          />
+          <Banner
+            title='There is a problem with your application'
+            status='error'
+          />
+          <Banner
+            title='Update available'
+            status='neutral'
+          />
+        </div>
+      </div>
     </>
   )
 }
--- advanced-react/banners/src/components/Banner.jsx
+++ advanced-react/banners/src/components/Banner.jsx
@@ -1,7 +1,31 @@
-export default function Badge({
-    children
-}) {
+import classnames from 'classnames';
+import { FaCheckCircle, FaExclamationTriangle } from "react-icons/fa";
+import { FaCircleXmark } from "react-icons/fa6";
+import { HiInformationCircle } from "react-icons/hi";
+
+const iconMap = {
+    success: <FaCheckCircle />,
+    warning: <FaExclamationTriangle />,
+    error: <FaCircleXmark />,
+    neutral: <HiInformationCircle />
+};
+
+export default function Banner({ title, text, status, className = '' }) {
+    const icon = iconMap[status] || iconMap['neutral'];
+
+    const allClasses = classnames(
+        'banner',
+        `banner-${status}`,
+        className
+    );
+
     return (
-        <div>{children}</div>
+        <div className={allClasses}>
+            <span className="icon">{icon}</span>
+            <div className="banner-header">
+                <h1>{title}</h1>
+                { text && <p>{text}</p> }
+            </div>
+        </div>
     )
 }
