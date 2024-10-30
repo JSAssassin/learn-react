80a7a0afe5287b8fe88546accab4aa28a98c3074
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-10-30 15:47:39 -0400

    Update card component.

--- advanced-react/cards/src/App.jsx
+++ advanced-react/cards/src/App.jsx
@@ -1,7 +1,18 @@
 import Card from './components/Card'
 
 function App() {
-  return <Card />
+  return (
+    <div className='app-container'>
+      <h1 className='app-title'>Cards</h1>
+      <Card size='lg'>
+        <h2 className='card-title'>Easy Deployment</h2>
+        <p className='card-text'>
+          Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et
+          magna sit morbi lobortis.
+        </p>
+      </Card>
+    </div>
+  )
 }
 
 export default App
--- advanced-react/cards/src/components/Card.jsx
+++ advanced-react/cards/src/components/Card.jsx
@@ -1,7 +1,16 @@
-export default function Card() {
+import { IoCloudUploadOutline } from "react-icons/io5";
+import classnames from 'classnames';
+
+export default function Card({children, icon=<IoCloudUploadOutline />,  className = '', size = 'md', iconBg = ''}) {
+    const allClasses = classnames(`card-${size}`, className);
+    const styles = {
+        backgroundColor: iconBg || '#3F75FE'
+    }
     return (
-        <div>
-            Card
+        <div className={`card ${allClasses}`}>
+            <div className="hover-text">Hover</div>
+            <div className={`icon icon-${size}`} style={styles}>{icon}</div>
+            {children}
         </div>
     )
 }
