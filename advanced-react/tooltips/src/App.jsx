977c147a2fd8d21ccc16dde94cc82ee128d89d85
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-11-01 14:59:30 -0400

    Add color themes for tooltip.

--- advanced-react/tooltips/src/App.jsx
+++ advanced-react/tooltips/src/App.jsx
@@ -2,9 +2,29 @@ import Tooltip from './components/Tooltip'
 
 function App() {
   return (
-    <>
-      <Tooltip />
-    </>
+    <div className='tooltip-container'>
+      <h1 className='title'>tooltip</h1>
+      <Tooltip
+        title='Archive notes'
+        text='Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.'
+        theme='avacado'
+      />
+      <Tooltip
+        title='Archive notes'
+        text='Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.'
+        theme='default'
+      />
+      <Tooltip
+        title='Archive notes'
+        text='Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.'
+        theme='ocean'
+      />
+      <Tooltip
+        title='Archive notes'
+        text='Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.'
+        theme='peach'
+      />
+    </div>
   )
 }
 
--- advanced-react/tooltips/src/components/Tooltip.jsx
+++ advanced-react/tooltips/src/components/Tooltip.jsx
@@ -1,5 +1,26 @@
-export default function Tooltip() {
+import { TbInbox } from "react-icons/tb";
+import classnames from 'classnames';
+
+const themes = ['avacado', 'peach', 'ocean', 'default'];
+
+export default function Tooltip({
+    icon=<TbInbox />, title, text, className='', theme='default'
+}) {
+    if(!themes.includes(theme)) {
+        theme = 'default'
+    }
+    const allClasses = classnames(
+        'dialogue-box',
+        theme,
+        className
+    );
     return (
-       <></>
+        <div className={allClasses}>
+            <span className="icon">{icon}</span>
+            <div className="tooltip-header">
+                <h1>{title}</h1>
+                <p>{text}</p>
+            </div>
+        </div>
     )
 }
