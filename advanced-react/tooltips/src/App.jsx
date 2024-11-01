22b712c50e9241e41b80bd9d4c9f57ff4e5d9531
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-11-01 12:06:17 -0400

    Add initial files for tooltips.

---
+++ advanced-react/tooltips/src/App.jsx
@@ -0,0 +1,11 @@
+import Tooltip from './components/Tooltip'
+
+function App() {
+  return (
+    <>
+      <Tooltip />
+    </>
+  )
+}
+
+export default App
---
+++ advanced-react/tooltips/src/data.js
@@ -0,0 +1,19 @@
+export const data = [
+    {
+        status: 'Success',
+        text: 'Your work has been saved'
+    },
+    {
+        status: 'Warning',
+        text: 'A network error was detected'
+
+    },
+    {
+        status: 'Information',
+        text: 'Please read updated information'
+    },
+    {
+        status: 'Error',
+        text: 'Please re-save your work again'
+    }
+]
